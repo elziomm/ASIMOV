650cd4c67440ccbe9e3d8d3febbf4c455974109a
Elzio Maia <elziomm@hotmail.com>
2020-02-01 11:20:14 -0300

    adequação de responsividade

--- assets/js/about.js
+++ assets/js/about.js
@@ -29,14 +29,14 @@ const target = document.querySelectorAll('[data-anime');
 const animationClass = 'animate';
 
 function animeScroll(){
-    const windowTop = window.pageYOffset + ((window.innerHeight * 4) / 4);
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
     target.forEach(function(element){
 
-        if((windowTop) > (element.offsetTop) ){
-            element.classList.add(animationClass);
-        } else {
-            element.classList.remove(animationClass);
-        }
+	    if((windowTop) > (element.offsetTop) ){
+	        element.classList.add(animationClass);
+	    } else {
+	        element.classList.remove(animationClass);
+	    }
     })
 }
 
